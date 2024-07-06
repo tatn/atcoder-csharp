3d155e0d82d26f1073076e68729371c1fbd50810
tatn <tatn@users.noreply.github.com>
2024-07-06 23:27:44 +0900

    A - Biscuits

---
+++ AGC/AGC017/agc017_a.cs
@@ -0,0 +1,23 @@
+﻿namespace AtCoderCsharp.AGC.AGC017
+{
+    internal class agc017_a
+    {
+        public static void Main(string[] args)
+        {
+            int[] ReadIntArray() => Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            int[] NP = ReadIntArray();
+            int N = NP[0];
+            int P = NP[1];
+
+            int[] A = ReadIntArray();
+
+            int evenCount = A.Where(x => x % 2 == 0).Count();
+            int oddCount = N - evenCount;
+
+
+
+            Console.WriteLine(1);
+        }
+    }
+}
