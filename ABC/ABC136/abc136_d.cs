cbabb6409d10ea960ed0b22580af980953195a00
tatn <tatn@users.noreply.github.com>
2024-08-22 10:15:07 +0900

    AtCoder Beginner Contest 136 D - Gathering Children

--- ABC/ABC136/abc136_d.cs
+++ ABC/ABC136/abc136_d.cs
@@ -9,6 +9,84 @@
             string S = ReadString();
 
 
+            int[] rContinueCount = new int[S.Length];
+            int countR = 0;
+
+            for (int i = 0; i < S.Length - 1; i++)
+            {
+                char current = S[i];
+                char next = S[i + 1];
+
+
+                if(current == 'R')
+                {
+                    countR++;
+                }
+                else
+                {
+                    countR = 0;
+                }
+
+                if(current == 'R' && next == 'L')
+                {
+                    rContinueCount[i] = countR;
+                }
+                else
+                {
+                    rContinueCount[i] = 0;
+                }
+            }
+
+
+            int[] lContinueCount = new int[S.Length];
+            int countL = 0;
+
+            for (int i = S.Length - 1; 0 < i; i--)
+            {
+                char current = S[i];
+                char prev = S[i - 1];
+
+
+                if (current == 'L')
+                {
+                    countL++;
+                }
+                else
+                {
+                    countL = 0;
+                }
+
+                if (current == 'L' && prev == 'R')
+                {
+                    lContinueCount[i] = countL;
+                }
+                else
+                {
+                    lContinueCount[i] = 0;
+                }
+            }
+
+            int[] answer = new int[S.Length];
+
+            for (int i = 0; i < S.Length - 1; i++)
+            {
+                int r = rContinueCount[i];
+
+                if(r == 0)
+                {
+                    continue;
+                }
+
+                int l = lContinueCount[i + 1];
+
+                int leftCount = ((r+1) / 2) + (l / 2);
+                int rightCount = (r / 2) + ( (l+1) / 2);
+
+                answer[i] = leftCount;
+                answer[i + 1] = rightCount;
+            }
+
+            Console.WriteLine(String.Join(" ",answer));
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿AtCoderCsharp.ABC.ABC366.abc366_d.Main(new string[] { });
+﻿AtCoderCsharp.ABC.ABC136.abc136_d.Main(new string[] { });
 //AtCoderCsharp.AGC.AGC002.agc002_b.Main(new string[] { });
 //AtCoderCsharp.OtherContests.keyence2020.keyence2020_c.Main(new string[] { });
 
