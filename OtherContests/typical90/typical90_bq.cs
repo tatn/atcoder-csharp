2692b39c46b59608a2413054817254a277b09058
tatn <tatn@users.noreply.github.com>
2024-09-25 07:47:05 +0900

    069 Colorful Blocks 2（★3） 070 Plant Planning（★4）

--- OtherContests/typical90/typical90_bq.cs
+++ OtherContests/typical90/typical90_bq.cs
@@ -6,7 +6,65 @@
     {
         public static void Main(string[] args)
         {
+            long[] ReadLongArray() => Console.ReadLine()!.Split().Select(long.Parse).ToArray();
 
+            long[] NK = ReadLongArray();
+            long N = NK[0];
+            long K = NK[1];
+
+            if (K == 1)
+            {
+                Console.WriteLine(N == 1L ? 1 : 0);
+                return;
+            }
+
+            if (K == 2)
+            {
+                Console.WriteLine(3L <= N ? 0 : 2);
+                return;
+            }
+
+            if (K == 3)
+            {
+                Console.WriteLine(N == 1L ? 3 : 6);
+                return;
+            }
+
+            if(N == 1)
+            {
+                Console.WriteLine(K);
+                return;
+            }
+
+            long answer = K * (K - 1L);
+            long mod = 1000_000_007L;
+            answer %= mod;
+
+            if (N == 2)
+            {
+                Console.WriteLine(answer);
+                return;
+            }
+
+
+
+            // K-2を　N-2乗する
+            long baseNumber = K - 2L;
+            long exponent = N - 2L;
+            while (0 < exponent)
+            {
+                if (exponent % 2 == 1)
+                {
+                    answer *= baseNumber;
+                    answer %= mod;
+                }
+
+                exponent /= 2;
+                baseNumber *= baseNumber;
+                baseNumber %= mod;
+            }
+
+            Console.WriteLine(answer);
         }
 
     }
--- OtherContests/typical90/typical90_br.cs
+++ OtherContests/typical90/typical90_br.cs
@@ -1,11 +1,30 @@
 ﻿namespace AtCoderCsharp.OtherContests.typical90
 {
-    // 070 - Plant Planning（★4） 
+    // 070 - Plant Planning（★4） ToDo
 
     internal class typical90_br
     {
         public static void Main(string[] args)
         {
+            int ReadInt() => int.Parse(Console.ReadLine()!);
+            int[] ReadIntArray() => Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            int N = ReadInt();
+
+            int[] X = new int[N + 1];
+            int[] Y = new int[N + 1];
+
+            for (int i = 1; i <= N; i++)
+            {
+                int[] xy = ReadIntArray();
+                X[i] = xy[0];
+                Y[i] = xy[1];
+
+            }
+
+
+
+
 
         }
 
